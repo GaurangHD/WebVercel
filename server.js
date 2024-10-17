9d345e6e2dc8b842462de17268f85b27c9315f2a
GaurangHD <gdhameliya2000@gmail.com>
2024-10-17 13:52:09 -0400

    Route Gaurang

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
- 
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+    res.send('Hello World!');
 });
- 
+
+app.get('/gaurang', (req, res) => {
+    res.send('Hello World!, I am Gaurang');
+});
+
 app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
+    console.log(`App listening on port ${port}`);
 });
